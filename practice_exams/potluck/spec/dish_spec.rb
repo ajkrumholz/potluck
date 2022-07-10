a15b43eab946816527e2b7e0d5fcb26b01bfd96d
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-10 08:56:09 -0400

    create dish class

---
+++ practice_exams/potluck/spec/dish_spec.rb
@@ -0,0 +1,20 @@
+require './lib/dish'
+
+RSpec.describe Dish do
+
+    before(:each) do
+        @dish = Dish.new("Couscous Salad", :appetizer)
+    end
+
+    it 'exists' do
+        expect(@dish).to be_instance_of(Dish)
+    end
+
+    it 'has a name' do
+        expect(@dish.name).to eq("Couscous Salad")
+    end
+
+    it 'has a category' do
+        expect(@dish.category).to eq(:appetizer)
+    end
+end
